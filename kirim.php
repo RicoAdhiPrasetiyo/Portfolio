53f9e84439be350b5dd785d99e663a7a9b69b587
RicoAdhiPrasetiyo <rikoadhi@gmail.com>
2023-06-08 10:39:07 +0700

    first commit

--- kirim.php
+++ kirim.php
@@ -1,35 +1,35 @@
 <?php
+
 use PHPmailer\PHPmailer\PHPmailer;
-use PHPmailer\PHPmailer\Exception;
 
 require 'phpmailer/src/exception.php';
 require 'phpmailer/src/PHPmailer.php';
 require 'phpmailer/src/SMTP.php';
 
-if(isset($_POST["send"])){
+if (isset($_POST["send"])) {
     $mail = new PHPmailer(true);
-    
-    $mail -> isSMTP();
-    $mail -> Host = 'smtp.gmail.com';
-    $mail -> SMTPAuth = true;
-    $mail -> Username = 'rikoadhi@gmail.com';
-    $mail -> Password = 'zknqbfkjrqyedrhx';
-    $mail -> SMTPSecure = 'ssl';
-    $mail -> Port = 465;
 
-    $mail -> setFrom('rikoadhi@gmail.com');
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'rikoadhi@gmail.com';
+    $mail->Password = 'zknqbfkjrqyedrhx';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
 
-    $mail -> addAddress($_POST["email"]);
+    $mail->setFrom('rikoadhi@gmail.com');
 
-    $mail -> isHTML(true);
+    $mail->addAddress($_POST["email"]);
 
-    $mail -> Subject = $_POST["subject"];
-    $mail -> Body = $_POST["pesan"];
+    $mail->isHTML(true);
 
-    $mail -> send();
+    $mail->Subject = $_POST["subject"];
+    $mail->Body = $_POST["pesan"];
+
+    $mail->send();
 
     echo
-    "
+        "
     <script>
     alert('Email Terkirim');
     document.location.href = 'index.html';
@@ -37,4 +37,3 @@ if(isset($_POST["send"])){
     ";
 
 }
-?>
